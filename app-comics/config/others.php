bfc190c9e5e0921f1576373a1bba14d14899c267
robpap96 <robertopapagni1996@outlook.it>
2023-01-14 17:01:55 +0100

    fix

---
+++ app-comics/config/others.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    [
+        "service" => "DIGITAL COMICS",
+        "image" => "resources/img/buy-comics-digital-comics.png",
+    ],
+    [
+        "service" => "DC MERCHANDAISE",
+        "image" => "resources/img/buy-comics-merchandise.png",
+    ],
+    [
+        "service" => "SUBSCRIPTION",
+        "image" => "resources/img/buy-comics-subscriptions.png",
+    ],
+    [
+        "service" => "COMIC SHOP LOCATOR",
+        "image" => "resources/img/buy-comics-shop-locator.png",
+    ],
+    [
+        "service" => "DC POWER VISA",
+        "image" => "resources/img/buy-dc-power-visa.svg",
+    ],
+];
--- app-comics/routes/web.php
+++ app-comics/routes/web.php
@@ -16,28 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
     $comics = config("comics");
-    $others = [
-        [
-        "service" => "DIGITAL COMICS",
-        "image" => "resources/img/buy-comics-digital-comics.png",
-        ],
-        [
-        "service" => "DC MERCHANDAISE",
-        "image" => "resources/img/buy-comics-merchandise.png",
-        ],
-        [
-            "service" => "SUBSCRIPTION",
-            "image" => "resources/img/buy-comics-subscriptions.png",
-        ],
-        [
-            "service" => "COMIC SHOP LOCATOR",
-            "image" => "resources/img/buy-comics-shop-locator.png",
-        ],
-        [
-            "service" => "DC POWER VISA",
-            "image" => "resources/img/buy-dc-power-visa.svg",
-        ],
-    ];
+    $others = config("others");
 
-    return view('home', compact('comics', 'others'),);
+    return view('home', compact('comics', 'others'));
 })->name('home');
