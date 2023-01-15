19f9544608eb975561c8159c1229056cfbdd5f0e
robpap96 <robertopapagni1996@outlook.it>
2023-01-15 12:22:48 +0100

    aggiunto footer-bottom

--- app-comics/resources/views/partials/footer.blade.php
+++ app-comics/resources/views/partials/footer.blade.php
@@ -19,4 +19,35 @@
             </div>
         </div>
     </section>
+    <section class="footer-bottom">
+        <div class="container py-4">
+            <div class="row justify-content-between">
+                <div class="col-2">
+                    <button class="btn ">SIGN UP NOW!</button>
+                </div>
+                <div class="col-4 d-flex">
+                    <div class="footer-social d-flex">
+                        <div class="follow-us">
+                            FOLLOW US
+                        </div>
+                        <div class="facebook px-2">
+                            <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                        </div>
+                        <div class="twitter px-2">
+                            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                        </div>
+                        <div class="youtube px-2">
+                            <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                        </div>
+                        <div class="pinterest px-2">
+                            <a href="#"><i class="fa-brands fa-pinterest"></i></a>
+                        </div>
+                        <div class="locker px-2">
+                            <a href="#"><i class="fa-solid fa-location-dot"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </footer>
